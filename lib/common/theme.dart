7c5cde8923b82a63759250bd486cd15f13d92818
THEODORE YAPI <theodoreyapi@MacBook-Pro-de-THEODORE.local>
2023-04-24 12:20:50 +0000

    change color

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -73,5 +73,5 @@ AlertDialog alartDialog(String title, String content, BuildContext context) {
 class LoginFormStyle {}
 
 //Les differentes couleurs de l'application
-Color couleurFond = const Color.fromRGBO(225, 239, 216, 1.0);
+Color couleurFond = const Color.fromRGBO(147, 226, 55, .7);
 Color couleurTransparant = Colors.transparent;
