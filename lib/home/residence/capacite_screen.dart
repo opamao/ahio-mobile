0f478edcc75e8105ac79bd5aeca554cf551c8e91
opamao <theodoreyapi@gmail.com>
2023-06-23 00:03:51 +0000

    ok

--- lib/home/residence/capacite_screen.dart
+++ lib/home/residence/capacite_screen.dart
@@ -142,7 +142,7 @@ class _CapaciteScreenState extends State<CapaciteScreen> {
                             ),
                           ),
                         ],
-                      )
+                      ),
                     ],
                   ),
                   Row(
